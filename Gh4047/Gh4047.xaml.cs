cee9416919b40850cb5d4eb2a3f2b8a2f11bab18
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-10-12 00:53:50 +0900

    Initial commit

---
+++ Gh4047/Gh4047.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Akihiko.Xamarin.Forms.Xaml.Issues.Gh4047
+{
+    class MarkupExtension<T> : IMarkupExtension<T>
+    {
+        public T ProvideValue(IServiceProvider provider)
+        {
+            return default(T);
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider provider)
+        {
+            return default(T);
+        }
+    }
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Component
+    {
+        public byte Property { get; set; }
+    }
+}
---
+++ Gh4062/Gh4062.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Akihiko.Xamarin.Forms.Xaml.Issues.Gh4062
+{
+    public class Generic<T>
+    {
+    }
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Component
+    {
+        public Generic<int> Property { get; set; }
+    }
+}
