d38290d56f882062a44d1d087014c6776d2a2b82
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-12-11 20:19:41 +0900

    Add Akihiko.Xamarin.Forms.Xaml.Issues.Gh4238

---
+++ Gh4238/Gh4238.xaml.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using Xamarin.Forms.Xaml;
+
+namespace Akihiko.Xamarin.Forms.Xaml.Issues.Gh4238
+{
+    [TestFixture]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Component
+    {
+        public ArrayList Property { get; set; }
+
+        [Test]
+        public void Test()
+        {
+            InitializeComponent();
+            Assert.AreEqual(0f, Property[0]);
+        }
+    }
+}
